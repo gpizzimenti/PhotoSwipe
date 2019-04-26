35acafd46a3810d44796ba4944f8187bc38bd663
Giuseppe Pizzimenti <giuseppe.pizzimenti@gmail.com>
2019-04-26 20:01:55 +0200

    Change current item with animation

--- dist/photoswipe.js
+++ dist/photoswipe.js
@@ -1055,24 +1055,72 @@ var publicMethods = {
 
 
 	goTo: function(index) {
+            var itemsDiff;
 
-		index = _getLoopedId(index);
+            if (index == _currentItemIndex + 1) { //Next
+                itemsDiff = 1;
+            }
+            else { //Prev
+                itemsDiff = -1;
+            }
 
-		var diff = index - _currentItemIndex;
-		_indexDiff = diff;
+            var itemChanged;
+            if(!_mainScrollAnimating) {
+                _currZoomedItemIndex = _currentItemIndex;
+            }
 
-		_currentItemIndex = index;
-		self.currItem = _getItemAt( _currentItemIndex );
-		_currPositionIndex -= diff;
-		
-		_moveMainScroll(_slideSize.x * _currPositionIndex);
-		
+            var nextCircle;
 
-		_stopAllAnimations();
-		_mainScrollAnimating = false;
+            _currentItemIndex += itemsDiff;
 
-		self.updateCurrItem();
-	},
+            if(_currentItemIndex < 0) {
+                _currentItemIndex = _options.loop ? _getNumItems()-1 : 0;
+                nextCircle = true;
+            } else if(_currentItemIndex >= _getNumItems()) {
+                _currentItemIndex = _options.loop ? 0 : _getNumItems()-1;
+                nextCircle = true;
+            }
+
+            if(!nextCircle || _options.loop) {
+                _indexDiff += itemsDiff;
+                _currPositionIndex -= itemsDiff;
+                itemChanged = true;
+            }
+
+            var animateToX = _slideSize.x * _currPositionIndex;
+            var animateToDist = Math.abs( animateToX - _mainScrollPos.x );
+            var finishAnimDuration = 333; 
+
+            if(_currZoomedItemIndex === _currentItemIndex) {
+                itemChanged = false;
+            }
+
+            _mainScrollAnimating = true;
+
+            _shout('mainScrollAnimStart');
+
+            _animateProp('mainScroll', _mainScrollPos.x, animateToX, finishAnimDuration, framework.easing.cubic.out, 
+                _moveMainScroll,
+                function() {
+                    _stopAllAnimations();
+                    _mainScrollAnimating = false;
+                    _currZoomedItemIndex = -1;
+
+                    if(itemChanged || _currZoomedItemIndex !== _currentItemIndex) {
+                        self.updateCurrItem();
+                    }
+
+                    _shout('mainScrollAnimComplete');
+                }
+            );
+
+            if(itemChanged) {
+                self.updateCurrItem(true);
+            }
+
+            return itemChanged;
+
+        },
 	next: function() {
 		self.goTo( _currentItemIndex + 1);
 	},
